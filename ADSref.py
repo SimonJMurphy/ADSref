bfda939b1d07890348a49cf9627ae7f12334471e
Simon Murphy <simonmurphy66@hotmail.com>
2021-10-31 11:17:15 +1100

    Format the Sun (odot) symbol in latex

--- ADSref.py
+++ ADSref.py
@@ -54,6 +54,7 @@ def latex_title_greek(title):
     greek_dict = {"α":r"$\alpha$", "β":r"$\beta$", "γ":r"$\gamma$", "δ":r"$\delta$", "ε":r"$\epsilon$", "ζ":r"$\zeta$", "η":r"$\eta$", "ι":r"$\iota$", "θ":r"$\theta$", "κ":r"$\kappa$", "λ":r"$\lambda$", "μ":r"$\mu$", "ν":r"$\nu$", "ξ":r"$\xi$", "π":r"$\pi$", "ρ":r"$\rho$", "σ":r"$\sigma$", "τ":r"$\tau$", "φ":r"$\phi$", "χ":r"$\chi$", "ψ":r"$\psi$", "ω":r"$\omega$"}
     for key in greek_dict.keys():
         title = title.replace(key, greek_dict[key])
+    title = title.replace("<SUB>⊙</SUB>",r"$_{\odot}$")
     return title
 
 
